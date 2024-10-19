0eccd2a80b1a76f2a90ac7474f395aa3428018b2
BadlyacX <117196010+BadlyacX@users.noreply.github.com>
2024-10-20 02:20:21 +0800

    Delete servermanager-2024.10.20/src/main/java/com/badlyac/servermanager directory

--- servermanager-2024.10.20/src/main/java/com/badlyac/servermanager/Main.java
+++
@@ -1,9 +0,0 @@
-package com.badlyac.servermanager;
-
-import net.minecraftforge.fml.common.Mod;
-
-@Mod(Main.MODID)
-public class Main {
-    public static final String MODID = "servermanager";
-}
-
